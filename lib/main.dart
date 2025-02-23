ebdc93c1bb44a6d994e24123e18fdaa79619db37
Alex Ribeiro <alexetec.ribeiro@gmail.com>
2025-02-23 13:48:03 -0300

    atualiza esquema de cores e título da página inicial

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(0, 60, 58, 183),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Dio Project'),
     );
   }
 }
@@ -104,11 +106,12 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Voce clicou no botão:',
+              style: const TextStyle(color: Colors.red, fontSize: 20),
             ),
+
+            Text('$_counter', style: Theme.of(context).textTheme.headlineLarge),
           ],
         ),
       ),
